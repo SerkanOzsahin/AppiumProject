c637587a3ebcd8e7ecc72662677d41df2d769b17
Serkan <ozsahinserkan@gmail.com>
2023-12-03 10:17:42 +0300

    project started

---
+++ src/test/java/utilities/Hooks.java
@@ -0,0 +1,40 @@
+package utilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Hooks {
+
+    public static AndroidDriver<AndroidElement> androidDriver;
+
+    static DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    @BeforeMethod
+    public void setUp() {
+        try {
+            capabilities.setCapability("deviceName", ConfigReader.getProperty("deviceName"));
+            capabilities.setCapability("platformName", ConfigReader.getProperty("platformName"));
+            capabilities.setCapability("platformVersion", ConfigReader.getProperty("platformVersion"));
+            capabilities.setCapability("automationName", ConfigReader.getProperty("automationName"));
+            capabilities.setCapability("app", System.getProperty("user.dir") + ConfigReader.getProperty("app"));
+            capabilities.setCapability("noReset", true);
+            androidDriver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723"), capabilities);
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed Url Exception:" + e.getMessage());
+        }
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (androidDriver != null) {
+            androidDriver.closeApp();
+            androidDriver = null;
+        }
+    }
+}
