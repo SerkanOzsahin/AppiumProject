965e8a26270a4714e938104fe10dfea4fe02bf12
Serkan <ozsahinserkan@gmail.com>
2023-12-05 16:46:25 +0300

    logoutStep fix

--- src/test/java/stepDefinitions/logoutStep.java
+++ src/test/java/stepDefinitions/logoutStep.java
@@ -7,6 +7,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.logoutPage;
 import utilities.ConfigReader;
@@ -18,6 +20,7 @@ import java.util.List;
 public class logoutStep extends Hooks {
 
     logoutPage dc = new logoutPage();
+    WebDriverWait wait = new WebDriverWait(androidDriver, 10);
     ReusableMethods rm = new ReusableMethods();
 
     @Given("The user is logged in")
@@ -26,10 +29,11 @@ public class logoutStep extends Hooks {
         List<AndroidElement> tsButton = androidDriver.findElements(By.xpath("//android.widget.Button[@text=\"Techno Study\"]"));
         if (tsButton.size() > 0) {
             tsButton.get(0).click();
-            rm.waitFor(2);
+            rm.waitFor(1);
             androidDriver.findElement(By.xpath("//android.widget.Button[@text='CONTINUE']")).click();
-            rm.waitFor(2);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(dc.username));
         }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.username));
         androidDriver.findElement(dc.username).sendKeys(ConfigReader.getProperty("username"));
         androidDriver.findElement(dc.password).sendKeys(ConfigReader.getProperty("password"));
         androidDriver.findElement(dc.signButton).click();
@@ -37,20 +41,21 @@ public class logoutStep extends Hooks {
 
     @When("The user navigates to the Settings page in the Hamburger Menu")
     public void theUserNavigatesToTheSettingsPageInTheHamburgerMenu() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.hamburgerButton));
         androidDriver.findElement(dc.hamburgerButton).click();
-        rm.waitFor(2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.settingsButton));
         androidDriver.findElement(dc.settingsButton).click();
-        rm.waitFor(2);
     }
 
     @And("The user clicks on the Sign Out button")
     public void theUserClicksOnTheSignOutButton() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.signOutButton));
         androidDriver.findElement(dc.signOutButton).click();
-        rm.waitFor(2);
     }
 
     @Then("The user is redirected to the login page")
     public void theUserIsRedirectedToTheLoginPage() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.signButton));
         WebElement signButton = androidDriver.findElement(By.xpath("//android.widget.Button[@text=\"SIGN IN\"]"));
         Assert.assertTrue(signButton.isDisplayed());
         tearDown();
