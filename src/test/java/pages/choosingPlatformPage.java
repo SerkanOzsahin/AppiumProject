e8514a01d79e27249cc32f5f3a374e672d4aa7df
iremtorun <irmtorun@gmail.com>
2023-12-07 20:02:59 +0300

    US_1 done

--- src/test/java/pages/choosingPlatformPage.java
+++ src/test/java/pages/choosingPlatformPage.java
@@ -1,11 +1,19 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Hooks;
 
 public class choosingPlatformPage extends Hooks {
-
     public choosingPlatformPage() {PageFactory.initElements(androidDriver, this);}
+
+    public By liveButton=By.xpath(" //android.widget.Button[@text=\"Live\"] ");
+    public By continueButton=By.xpath(" //android.widget.Button[@text=\"CONTINUE\"] ");
+    public By technoStudyButton=By.xpath(" //android.widget.Button[@text=\"Techno Study\"] \n");
+    public By switchButton=By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[3]/android.widget.TextView");
+    public By demoButton=By.xpath(" //android.widget.Button[@text=\"Demo\"] ");
+    public By testButton=By.xpath(" //android.widget.Button[@text=\"Test\"] ");
+
 
 
 }
--- src/test/java/stepDefinitions/choosingPlatformStep.java
+++ src/test/java/stepDefinitions/choosingPlatformStep.java
@@ -1,24 +1,62 @@
 package stepDefinitions;
 
+import io.appium.java_client.android.AndroidElement;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import pages.choosingPlatformPage;
 import utilities.Hooks;
 import utilities.ReusableMethods;
 
+import java.util.List;
+
 public class choosingPlatformStep extends Hooks {
 
-    ReusableMethods rs =new ReusableMethods();
+
+    choosingPlatformPage dc = new choosingPlatformPage();
 
     @Given("The user launches the app")
     public void theUserLaunchesTheApp() {
+        setUp();
     }
 
     @When("The user chooses a platform")
     public void theUserChoosesAPlatform() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.liveButton));
+        androidDriver.findElement(dc.liveButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.continueButton));
+        androidDriver.findElement(dc.continueButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.switchButton));
+        androidDriver.findElement(dc.switchButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.technoStudyButton));
+        androidDriver.findElement(dc.technoStudyButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.continueButton));
+        androidDriver.findElement(dc.continueButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.switchButton));
+        androidDriver.findElement(dc.switchButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.demoButton));
+        androidDriver.findElement(dc.demoButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.continueButton));
+        androidDriver.findElement(dc.continueButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.switchButton));
+        androidDriver.findElement(dc.switchButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.testButton));
+        androidDriver.findElement(dc.testButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.continueButton));
+        androidDriver.findElement(dc.continueButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(dc.switchButton));
+        androidDriver.findElement(dc.switchButton).click();
+
+
     }
 
     @Then("The user should be directed to the platform")
     public void theUserShouldBeDirectedToThePlatform() {
+
+        tearDown();
     }
 }
